b51491991413562aa07c1740891ad04ef3f515b8
hexie2108 <blue.wolf4444444@gmail.com>
2020-03-01 11:36:12 +0100

    去除myfragment

--- App/app/src/main/java/org/mikuclub/app/ui/fragments/HomeCategoriesFragment.java
+++ App/app/src/main/java/org/mikuclub/app/ui/fragments/HomeCategoriesFragment.java
@@ -10,13 +10,13 @@ import org.mikuclub.app.config.GlobalConfig;
 import org.mikuclub.app.javaBeans.response.baseResource.Category;
 import org.mikuclub.app.storage.CategoryPreferencesUtils;
 import org.mikuclub.app.storage.UserPreferencesUtils;
-import org.mikuclub.app.ui.activity.base.MyFragment;
 import org.mikuclub.app.utils.RecyclerViewUtils;
 
 import java.util.List;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import mikuclub.app.R;
@@ -25,7 +25,7 @@ import mikuclub.app.R;
  * 主页-分类分页
  * Home page: category list fragment
  */
-public class HomeCategoriesFragment extends MyFragment
+public class HomeCategoriesFragment extends Fragment
 {
 
 
